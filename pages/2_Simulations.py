9405547a822f21ac287deaad5ece4f6132fdd96b
marinecourtois <marinecourtois2609@icloud.com>
2024-09-10 10:38:07 +0200

    Update 2_Simulations.py

--- pages/2_Simulations.py
+++ pages/2_Simulations.py
@@ -5,7 +5,7 @@ from utils.funTISapp import *
 
 st.set_page_config(layout="wide", page_title = "Simulations - Modèle Technique de l'Insecte Stérile", page_icon = "📈")
 
-st.sidebar.header("Simulations")
+st.sidebar.header("Simulations - Modèle Technique de l'Insecte Stérile")
 
 col2, col3 = st.columns([5, 15], gap = "large")
 
