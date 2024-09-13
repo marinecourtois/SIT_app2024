a29c88ec8287cb06ce7ff7789da84015de7dc510
marinecourtois <marinecourtois2609@icloud.com>
2024-09-13 11:29:33 +0200

    Update TIS.py

--- TIS.py
+++ TIS.py
@@ -5,7 +5,6 @@ from utils.funTISapp import *
 
 st.set_page_config(layout="wide", page_title = "Introduction", page_icon ="📚")
 st.sidebar.header("Introduction")
-st.title("intro")
 
 col2, col3 = st.columns([5, 15], gap = "large")
 
