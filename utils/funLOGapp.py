e4c0923b1b4387e2404300742396692a64aad3d5
julesguil <jules.guilberteau@inrae.fr>
2024-09-13 10:13:14 +0200

    Update funLOGapp.py

--- utils/funLOGapp.py
+++ utils/funLOGapp.py
@@ -18,23 +18,23 @@ pas_t = 0.01        # pas de temps de récupération des variables entre t_0 et 
 tspan = np.arange(t_0, t_fin, pas_t)
 
 # modele de croissance exponentielle
-def model_exp(etat, t, params):
+def model_log(etat, t, params):
     N = etat
-    r, μ = params
-    Ndot = [(r-μ)*N[0]]
+     alpha, r, μ = params
+    Ndot = [r*(1-alpha*N[0])*N[0]-µ*N[0]]
     return Ndot    
 
 # fonction pour intégration et plot des dynamiques
-def plotSim(N0, r, μ, tspan = tspan):
-    params = np.array([r, μ])
+def plotSim(N0, alpha, r, μ, tspan = tspan):
+    params = np.array([apha, r, μ])
     
-    int_exp = odeint(model_exp, N0, tspan, args=(params,), hmax=pas_t)
+    int_exp = odeint(model_log, N0, tspan, args=(params,), hmax=pas_t)
     
     # figure
     fig1, ax1 = plt.subplots(figsize=(8, 6))  
 
     # tracé des simulations par rapport au temps
-    ax1.plot(tspan, int_exp[:,0], color='C0', label='Taille de la population (N)', linewidth = 2)
+    ax1.plot(tspan, int_log[:,0], color='C0', label='Taille de la population (N)', linewidth = 2)
     
     ax1.legend(fontsize='10', loc = 'upper right')
     ax1.set_xlabel('temps', fontsize='12')
