5765fd8bcbf3a2ca07eaa38603ca8b25d7e7cb85
marinecourtois <marinecourtois2609@icloud.com>
2024-09-13 10:27:18 +0200

    Update 2_Modèle exponentiel.py

--- pages/2_Modèle exponentiel.py
+++ pages/2_Modèle exponentiel.py
@@ -25,13 +25,13 @@ with col121:
 
 col22, col23, col24 = st.columns([7, 7, 7], gap = "large")
 with col22:
-    N0 = st.slider(' Densité initiale', min_value = 0., max_value = 10., value = 5., step=1.)  
+    N0 = st.slider(r' Densité initiale ($N_0$)', min_value = 0., max_value = 10., value = 5., step=1.)  
 
 with col23:
-    r = st.slider(' Taux de natalité', min_value = 0., max_value = 1., value = 0.5, step=0.05) 
+    r = st.slider(' Taux de natalité (r)', min_value = 0., max_value = 1., value = 0.5, step=0.05) 
 
 with col24:
-    μ =  st.slider(' Taux de mortalité', min_value = 0., max_value = 1., value = 0.5, step = 0.05)
+    μ =  st.slider(' Taux de mortalité (μ)', min_value = 0., max_value = 1., value = 0.5, step = 0.05)
 
 ### plots
 
